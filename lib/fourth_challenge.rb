7a2886d8f1c4477367928ce50c61c79eeeb9649c
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-07 20:43:55 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,6 +9,11 @@ def fourth_challenge
           :name => "Benvolio",
           :age => "17",
           :attitude => "worried"
+        },
+        { :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-headed"
+        }
         }
       ]
    }, 
