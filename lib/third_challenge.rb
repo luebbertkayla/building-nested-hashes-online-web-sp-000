c5a2bfe5e9378ddc80eba57eebdbc1cd2612c18b
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-07 20:35:39 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,15 +10,16 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        :name: => "Romeo"
-        :age => "15"
+        :name: => "Romeo",
+        :age => "15",
         :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Capulet",
+        
         },
       :matriarch => {
         #your key/value pairs here
