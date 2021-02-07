08d23a7e9538341a2277716ea4e129d6046dbe5e
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-07 20:25:40 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,8 +7,9 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      
-      }}
-
-  
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+      }
 end
